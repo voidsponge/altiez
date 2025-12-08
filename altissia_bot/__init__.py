634357a74a4d439674ac0672a91f45de20ee0627
Tokzeen <goster013@gmail.com>
2025-12-08 10:40:28 +0100

    feat: differents exo types & methods & auto retry

--- altissia_bot/__init__.py
+++ altissia_bot/__init__.py
@@ -2,6 +2,6 @@
 Altissia Bot - Automation for Altissia language exercises
 """
 
-__version__ = "1.0.0"
-__author__ = "Leo"
+__version__ = "1.0.1"
+__author__ = "Leo & Enzo"
 __description__ = "Automation bot for Altissia language exercises using Python and Playwright"
